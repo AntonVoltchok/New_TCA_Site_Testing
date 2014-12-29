9e1560633ff7fb2f75db1e789037871c8761ab5f
Anton Voltchok <antonvoltchok1@gmail.com>
2014-12-29 06:56:23 -0500

    Menu done, FA animation dev code change
    next step, add jquery transition to anchors on page, finish setting up
    SASS media query mixins

--- js/main.js
+++ js/main.js
@@ -1,15 +1,54 @@
-  $('#nav-button').click(function(){
-//    $('.skype').fadeIn(2000);
-	$('.practice-fade').fadeIn(1000);
+//First time trying to animate jquery
+
+/*
+  $('#navvvv-button').click(function(){
+    $('.skype').fadeIn(2000);
+	$('.practiceeee-fade').fadeIn(1000);
   });
 
+*/
 
+
+//Below is a really useful snippet to stop 
+//making anchor tags scroll to top of page
+
+/*
+( function( $ ) {
+   $( 'a[href="#"]' ).click( function(e) {
+      e.preventDefault();
+   } );
+} )( jQuery );
+
+*/
+
+
+// MENU / NAVICON EXAMPLE
+/*
 $('#nav-button').click( function(){
   $(this).toggleClass('width');
   $(this).parent().toggleClass('show');
   $(this).children().toggleClass( 'fa-navicon').toggleClass( 'fa-close');
   $('#nav-list').toggleClass('nav-show'); 
 });
+*/
+
+
+
+// BASIC SLIDER
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
