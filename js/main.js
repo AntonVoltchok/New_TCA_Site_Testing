48e62bf3ae61d74651dbac2ed70ec0a9176ed583
Anton Voltchok <antonvoltchok1@gmail.com>
2015-01-15 16:37:53 -0500

    All section structure done, next: fitvids / modals

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+ // Semicolon (;) to ensure closing of earlier scripting
+    // Encapsulation
+    // $ is assigned to jQuery
+    ;(function($) {
+
+         // DOM Ready
+        $(function() {
+
+            // Binding a click event
+            // From jQuery v.1.7.0 use .on() instead of .bind()
+            $('#my-button').bind('click', function(e) {
+
+                // Prevents the default action to be triggered. 
+                e.preventDefault();
+
+                // Triggering bPopup when click event is fired
+                $('#element_to_pop_up').bPopup();
+
+            });
+
+        });
+
+    })(jQuery);
+
+
+
+jQuery(document).ready(function($) {
+	$('.counter').counterUp({
+		delay: 10,
+		time: 1000
+	});
+});
+
 
 // MENU
 
@@ -63,6 +96,8 @@ $(function() {
   });
 
 });
+
+
 
 
 
